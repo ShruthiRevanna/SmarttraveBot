6fb033510c3e3e1ff709012078280928e0c68e6f
ShruthiRevanna <shruthir.157'gmail.com>
2018-08-21 17:19:41 +0100

    API

---
+++ app/controllers/node.controller.js
@@ -0,0 +1,57 @@
+const Product = require('../models/node.model');
+
+// controllers/products.js
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            line: req.body.line,
+            status: req.body.status
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+// controllers/products.controller.js
+exports.product_details = function (req, res) {
+
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        //  res.send(product);
+    })
+    //if (req.body.result.action == "faq-delivery"){
+
+    return res.json({
+        speech: 'Here is the status',
+        displayText:'Feeling happy',
+        source: 'My firs webhoook'
+    });
+   // }
+
+};
+
+// controllers/products.controller.js
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+// controllers/products.controller.js
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAADdpXk5eMcBAKRvsIXPSCNeuJgiMUfCWJuKZBxYXsZAH7lYKrDAIrECQZAdVTbJOiYpKcIAUVvGVUiUzxtGHULZCW8Jy9PX2m5E1SPgBktcrizpTe6R6twhlp1wO7u8QyZAgWk9oeOza4PwO22kZA92PqOr7JLOzRVZCRwXieZCa24gPR1mzrkaTkFN9OonZAMUZD',
+    FB_VERIFY_TOKEN: 'smarttravelbotforprojectlsbu',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c194566f04984f568aa56a74ffa0b838',
+    FB_APP_SECRET: '4d0d36bcdef5633a66b1296b9d67d4b9',
+    SERVER_URL: "https://smarttravelbot.herokuapp.com/",
+    TFL_API_KEY: "b21d800dcc590d2ab1613d72d6ef0dfe",
+    TFL_API_ID: "82fa9e50",
+};
+
