a3b03e171fc0ddc6dc861eb004e179e98f4d066a
ShruthiRevanna <shruthir.157'gmail.com>
2018-08-22 21:54:13 +0100

    comm

--- app.js
+++ app.js
@@ -383,7 +383,7 @@ function handleApiAiResponse(sender, response) {
 
     sendTypingOff(sender);
 
-    if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
+    if (isDefined(messages)){//} && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
         let timeoutInterval = 1100;
         let previousType ;
         let cardTypes = [];
