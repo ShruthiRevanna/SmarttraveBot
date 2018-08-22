848e0b71f3ab4b32095169ab970c968d2c70d93c
ShruthiRevanna <shruthir.157'gmail.com>
2018-08-22 18:22:26 +0100

    qs parameters

--- app.js
+++ app.js
@@ -228,11 +228,11 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 
                   request.get({
                      url : "https://api.tfl.gov.uk/line/mode/tube/status",
-                      /*qs  : {
+                       qs  : {
                           appid: config.TFL_API_ID,
                           appkey: config.TFL_API_KEY,
                           //qstatus: parameters["underground_line"],
-                         },*/
+                         },
                       },function(error,response,body){
                       if(!error && response.statusCode == 200){
                             //let name = Bakerloo;
