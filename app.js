6e8b1085a68efefb554a30bb8b6098b514d57443
ShruthiRevanna <shruthir.157'gmail.com>
2018-08-23 09:57:02 +0100

    if to good service

--- app.js
+++ app.js
@@ -243,12 +243,13 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
                             if (bus[bus_num]["id"] == bus_id){
                                 let status = bus[bus_num]["lineStatuses"][0]["statusSeverityDescription"];
                                 console.log(status);
-                                //console.log(reason);
-                               // if (status == "Good Service") {
+
+                                if (status == "Good Service") {
                                     let reply = bus[bus_num]["lineStatuses"][0]["statusSeverityDescription"];
-                              //  } else if (status == "Special service"){
-                                    //let reply = bus[bus_num]["lineStatuses"][0]["reason"];
+                                } //else if (status == "Special service"){
+                                  //  let reply = bus[bus_num]["lineStatuses"][0]["reason"];
                                // }
+                                // console.log(reason);
                                 sendTextMessage(sender, reply);
                                 console.log("Reply response");
                                 console.log(reply);
